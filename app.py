e9a928e99d98831144fc2cb4c611a94443fcb846
iliasathan35-gr <iliasathan35@gmail.com>
2026-05-05 15:32:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -56,5 +56,8 @@ def admin():
 def success():
     return "Το ραντεβού κλείστηκε!"
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
