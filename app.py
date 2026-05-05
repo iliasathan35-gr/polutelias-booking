0b05f47a3c5397bba0e2c42527c3d59fad17f941
iliasathan35-gr <iliasathan35@gmail.com>
2026-05-05 22:21:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def send_telegram(text):
     try:
         import requests
 
-      TELEGRAM_TOKEN = "8780779879:AAHKpT6H0aLiWQV85-08NvWh3l_xBEyHfLA"
-CHAT_ID = "8780021902"
+    TELEGRAM_TOKEN = "8780779879:AAHKpT6H0aLiWQV85-08NvWh3l_xBEyHfLA"
+    CHAT_ID = "8780021902"
 
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
         requests.post(url, data={"chat_id": CHAT_ID, "text": text})
