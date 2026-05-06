140401c3e5561339f3542d73771a01157d232851
iliasathan35-gr <iliasathan35@gmail.com>
2026-05-06 21:10:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -187,7 +187,6 @@ def admin():
     day = today + timedelta(days=i)
     date_str = day.strftime("%Y-%m-%d")
 
-    # 👉 ΝΕΟ (label με ελληνικά)
     label = f"{days_gr[day.weekday()]} {day.day} {months_gr[day.month - 1]} {day.year}"
 
         slots = generate_slots(day.weekday())
