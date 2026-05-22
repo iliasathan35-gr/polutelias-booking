45bf581caea0835a1b39a7803232ac516d62f33e
iliasathan35-gr <iliasathan35@gmail.com>
2026-05-22 14:38:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -597,23 +597,24 @@ def subscribe():
     save_push_subscriptions(subs)
 
     return {"success": True}
- def send_push_to_phone(phone, title, body):
+def send_push_to_phone(phone, title, body):
      
-     phone = phone.replace(" ", "").strip()
+    phone = phone.replace(" ", "").strip()
 
-     print("SEND PUSH FUNCTION STARTED", flush=True)
-     print("SEND PUSH TO:", phone, flush=True)
+    print("SEND PUSH FUNCTION STARTED", flush=True)
+    print("SEND PUSH TO:", phone, flush=True)
 
-     subs = load_push_subscriptions()
+    subs = load_push_subscriptions()
 
-     for item in subs:
+    for item in subs:
 
-         saved_phone = item.get("phone", "").replace(" ", "").strip()
+        saved_phone = item.get("phone", "").replace(" ", "").strip()
 
-         if saved_phone == phone:
+        if saved_phone == phone:
 
-             print("FOUND SUB:", item, flush=True)
+            print("FOUND SUB:", item, flush=True)
 
+           
             try:
 
                 webpush(
